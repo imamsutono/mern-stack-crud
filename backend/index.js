20537d216e0dce90859c0cf67469b0084394e48b
Imam Sutono <imamsutono>
2023-10-20 16:45:00 +0700

    change localhost to ip

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ import UserRoute from "./routes/UserRoute.js";
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/fullstack_db', {
+mongoose.connect('mongodb://127.0.0.1:27017/fullstack_db', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
